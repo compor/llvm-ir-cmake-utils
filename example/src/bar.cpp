0860f8486f35e7260ba0d9a8f7ae1396c3fad89f
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-07-27 12:26:08 +0100

    add example usage

---
+++ example/src/bar.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+#include "bar.hpp"
+
+
+void foo() {
+  std::cout << "foo\n";
+
+  return;
+}
+
+
---
+++ example/src/foo.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+#include "bar.hpp"
+
+
+int main(int argc, const char *argv[]) {
+  std::cout << "Hello!\n";
+  foo();
+
+  return 0;
+}
+
